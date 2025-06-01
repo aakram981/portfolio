07fa1f09894778338e81fa52b9b42bb217931679
somebodyawesome-dev <luckynoob2011830@gmail.com>
2025-06-01 22:01:29 +0100

    update router baseurl

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-import About from './components/About';
-import Projects from './components/Project';
-import Contact from './components/Contact';
-import CV from './pages/CV'; 
+import About from "./components/About";
+import Projects from "./components/Project";
+import Contact from "./components/Contact";
+import CV from "./pages/CV";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <div className="pt-36"> 
-        <Routes>
+      <div className="pt-36">
+        <Router basename="/portfolio">
           <Route path="/" element={<About />} />
           <Route path="/cv" element={<CV />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/Contact" element={<Contact />} />
-
-        </Routes>
+        </Router>
       </div>
       <Footer />
     </Router>
